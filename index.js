997fff61a7a8466301cc328cf30406bba2c8b77e
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-05 00:44:43 +0530

    fix: color changer function added

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ var name=document.getElementsByTagName('h1')[0];
 
 name.style.color='red';
 
+
+function changeColor() {
+    var random=Math.floor(Math.random()*7);
+    var pattern='0123456789ABCDEF';
+    var first='#';
+    for (let index = 0; index < 6; index++) {
+        var ele=pattern[random];
+        first=first+ele;
+        
+    }
+    return first;
+}
+
+
+alert(changeColor());
